99278bbca32f1737b5dce0f112a9a1bb8d63f411
V-Mall-OS <84397668+V-Mall-OS@users.noreply.github.com>
2021-12-07 00:11:59 +0100

    there was a cockroach in my code

--- docker/konfigurator/app/getIP.php
+++ docker/konfigurator/app/getIP.php
@@ -3,8 +3,8 @@
 session_start();
 // nehme die Variable shopname aus der Session um dadurch an den Namespace zu gelangen
 $shopname = $_SESSION["shopname"] ;
-$shopnamen = preg_replace('/\s/', '', $shopname);
-$shopnamen = strtolower(trim($shopnamen))
+$shopnamen = $shopname;
+$shopnamen = strtolower(trim(preg_replace('/\s+/', '', $shopname)));
 // fordere öffentliche IP-Adresse vom bestimmten namespace
 $kubectlst = "./kubectl -n $shopnamen get service nginx -o jsonpath='{.status.loadBalancer.ingress[0].ip}'";
 $status = shell_exec($kubectlst);
@@ -12,5 +12,8 @@ $status = shell_exec($kubectlst);
 echo "Deine Adresse zu deinem V-Mall-Portal $shopname,\r\n";
 echo "ist jetzt unter folgender IP-Adresse erreichbar ->  ";
 echo "http://", $status,"\r\n";
+echo "Der Administratorenbereich ist unter folgender Adresse erreichbar ->";
+echo "http://", $status,"/admin","\r\n";
+echo "Benutzername:admin Passwort:admin";
 echo "VIEL VERGNÜGEN!!!!!!";
 ?>
